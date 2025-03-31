5c3ec077f68a1741a8b1db685f82d9802171a16c
Miami <alex.miami.petrov@gmail.com>
2025-03-31 18:55:32 +0300

    final_operations

--- task-4/contact_operations.py
+++ task-4/contact_operations.py
@@ -1,113 +1,88 @@
+from address_book import AddressBook, Record, Name, Phone, Birthday
+
 def validate_contact_args(args: list) -> str:
-    
     if len(args) != 2:
         return "Invalid command. Usage: [command] [ім'я] [номер телефону]"
-
     try:
         name, phone = args
         if not phone.isdigit():
             return "Invalid phone number. Phone number must contain only digits."
-
     except ValueError:
         return "Invalid command. Usage: [command] [ім'я] [номер телефону]"
-
     return None
-
 
 def input_error(func):
     def inner(*args, **kwargs):
         try:
             return func(*args, **kwargs)
-        except ValueError:
-            return "Invalid command. Usage: [command] [ім'я] [номер телефону]"
+        except ValueError as e:
+            return str(e)
         except KeyError:
             return "Contact not found"
         except IndexError:
             return "Invalid command. Usage: [command] [ім'я] [номер телефону]"
-        
     return inner
 
+@input_error
+def add_contact(args: list, book: AddressBook) -> str:
+    name, phone, *_ = args
+    record = book.find(name)
+    message = "Contact updated."
+    if record is None:
+        record = Record(Name(name))
+        book.add_record(record)
+        message = "Contact added."
+    if phone:
+        record.add_phone(Phone(phone))
+    return message
 
 @input_error
-def add_contact(args: list, contacts: dict) -> str:
-    if len(args) != 2:
-        raise IndexError
-    
-    name, phone = args
-
-    if not phone.isdigit():
-        raise ValueError
-    
-    contacts[name] = phone
-
-    return "Contact added."
-
-@input_error
-def change_contact(args: list, contacts: dict) -> str:
-    if len(args) != 2:
-        raise IndexError
-    
-    name, phone = args
-
-    if not phone.isdigit():
-        raise ValueError
-
-    if name in contacts:
-        contacts[name] = phone
+def change_contact(args: list, book: AddressBook) -> str:
+    name, old_phone, new_phone, *_ = args
+    record = book.find(name)
+    if record:
+        record.edit_phone(old_phone, new_phone)
         return "Contact updated."
-
     else:
         raise KeyError
 
 @input_error
-def show_phone(args: list, contacts: dict) -> str:
-
-    if len(args) != 1:
-        raise IndexError
-    
-    name_lower = args[0].lower()
-
-    for stored_name, phone in contacts.items():
-
-        if name_lower == stored_name.lower():
-            return phone
-    
-    raise KeyError
-
-@input_error
-def show_all(contacts: dict) -> str:
-
-    if not contacts:
-        return "No contacts saved."
-    
+def show_phone(args: list, book: AddressBook) -> str:
+    name, *_ = args
+    record = book.find(name)
+    if record:
+        return "; ".join(str(p.value) for p in record.phones)
     else:
-        result = ""
-        for name, phone in contacts.items():
-            result += f"{name}: {phone}\n"
+        raise KeyError
 
-        return result.strip()
-    
 @input_error
-def add_birthday(args, book: AddressBook)
+def show_all(book: AddressBook) -> str:
+    if not book.data:
+        return "No contacts saved."
+    else:
+        return "\n".join(str(record) for record in book.data.values())
+
+@input_error
+def add_birthday(args: list, book: AddressBook) -> str:
     name, birthday, *_ = args
     record = book.find(name)
     if record:
-        record.add_birthday(birthday)
-        return "Birthday added"
+        record.add_birthday(Birthday(birthday))
+        return "Birthday added."
     else:
         raise KeyError
-    
+
 @input_error
-def show_birthday(args, book: AddressBook):
+def show_birthday(args: list, book: AddressBook) -> str:
     name, *_ = args
     record = book.find(name)
     if record and record.birthday:
         return record.show_birthday()
     else:
         raise KeyError
-    
+
 @input_error
-def birthdays(args, book: AddressBook):
+def birthdays(args: list, book: AddressBook) -> str:
     return book.get_upcoming_birthdays()
     
 
